c7457f5c914ec37d68ae575a2940ba1493c731b1
tsaipei-linebot <gary@tsaipei.com>
2026-09-02 06:55:02 +0800

    Delete services/config.py

--- services/config.py
+++
@@ -1,5 +0,0 @@
-# ==========================================
-# Google Cloud / Vertex AI 設定
-# ==========================================
-GCP_PROJECT_ID = "tsaipei-505807"
-GCP_LOCATION = "asia-east1" 
